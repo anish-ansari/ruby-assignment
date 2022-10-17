e8ea3778d1b2b2826b9734169226c2f033c6b02d
Anish Ansari <anish.ansari@bajratechnologies.com>
2022-10-17 10:57:52 +0545

    Add time practice code

---
+++ 4. math, time, regex and hash/ex_4.2.1.rb
@@ -0,0 +1,4 @@
+moon_landing = Time.new(1969, 7, 20, 20, 17, 40)
+my_birthday = Time.new(2000, 01, 15, 04, 25, 16)
+seconds = my_birthday - moon_landing
+puts "I was born #{seconds} seconds after the moon landing"
