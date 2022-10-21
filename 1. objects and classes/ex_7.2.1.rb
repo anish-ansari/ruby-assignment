2bccf9cef0d6ee3c74622e07ee6e98dfdac4c714
Anish Ansari <anish.ansari@bajratechnologies.com>
2022-10-21 11:45:04 +0545

    Add class and objects exercises

---
+++ 1. objects and classes/ex_7.2.1.rb
@@ -0,0 +1,8 @@
+# Defines a Phrase class (inheriting from String).
+class Phrase < String
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content = downcase # removing self
+    processed_content == processed_content.reverse
+  end
+end
---
+++ 1. objects and classes/ex_7.5.1.rb
@@ -0,0 +1,5 @@
+arr = (1..100).to_a
+
+puts arr.max
+puts arr.min
+puts arr.reduce(:+)
