bbe2828b700997e6cec5a77d2be48882b3565694
Anish Ansari <anish.ansari@bajratechnologies.com>
2022-11-05 22:01:54 +0545

    Add database connection code

---
+++ 10. ruby with mysql/expense_tracker/db.rb
@@ -0,0 +1,19 @@
+require "mysql2"
+
+class Database
+  def db_connection
+    host = "localhost"
+    db = "expense_tracker"
+    user = "anish"
+    pass = "Anish786!@#"
+
+    # accessing the database via mysql2 gem
+    conn = Mysql2::Client.new(
+      host: host,
+      username: user,
+      database: db,
+      password: pass,
+    )
+    @conn = conn
+  end
+end
