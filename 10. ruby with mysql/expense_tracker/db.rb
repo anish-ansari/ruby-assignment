2d606eeedc8d02c0312ebe43fab62b0919f82fbc
Anish Ansari <anish.ansari@bajratechnologies.com>
2022-11-20 20:22:45 +0545

    Add minor changes

--- 10. ruby with mysql/expense_tracker/db.rb
+++ 10. ruby with mysql/expense_tracker/db.rb
@@ -1,18 +1,18 @@
-require "mysql2"
+require 'mysql2'
 
 class Database
   def db_connection
-    host = "localhost"
-    db = "expense_tracker"
-    user = "anish"
-    pass = ""
+    host = 'localhost'
+    db = 'expense_tracker'
+    user = 'anish'
+    pass = ''
 
     # accessing the database via mysql2 gem
     conn = Mysql2::Client.new(
       host: host,
       username: user,
       database: db,
-      password: pass,
+      password: pass
     )
     @conn = conn
   end
